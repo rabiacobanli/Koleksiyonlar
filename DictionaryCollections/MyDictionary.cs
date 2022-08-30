f1d1fb1733f83cb12438cbee167c838067fcb466
Rabia ÇOBANLI <r.cobanli95@hotmail.com>
2022-08-30 13:12:16 +0300

    Dictionary eklendi.

---
+++ DictionaryCollections/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollections
+{
+    public class MyDictionary<T,K>
+    {
+        KeyValuePair<T,K>[] items;
+        public MyDictionary()
+        {
+            items = new KeyValuePair<T, K>[0];
+        }
+        public void Add(T key,K value)
+        {
+            KeyValuePair<T, K>[] tempArray = items;
+            items = new KeyValuePair<T, K>[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = new KeyValuePair<T, K>(key, value);
+        }
+        public void List()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+        }
+        
+    
+    }
+}
---
+++ DictionaryCollections/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using DictionaryCollections;
+
+MyDictionary<int, string> names = new MyDictionary<int, string>();
+names.Add(1, "Rabia");
+names.List();
